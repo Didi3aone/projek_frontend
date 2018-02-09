1ac07730c7b1597b34759332c133afae0fb3d7e4
budiamanprasetyo <setyaamanbudi@gmail.com>
2018-02-09 08:39:48 +0700

    perbaikan struktur file-file

--- view/content.php
+++ view/content.php
@@ -1,60 +1,15 @@
-<div class="row">
-
-	<p>SELECT PROFILE</p>
-	<div class="col-md-4">
-		<div class="panel panel-primary" >
-			<div class="panel-heading">
-				<div class="panel-title">Personal</div>
-				<!-- <div class="col-md-4">dd</div> -->
-				<!-- <p style="margin-left: 300px; margin-top: 1px;">e</p> -->
-			</div>     
-			<div style="padding-top:30px" class="panel-body" >
-				<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-				<form id="loginform" class="form-horizontal" role="form">
-					<div style="margin-bottom: 25px" class="input-group">
-						<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-						<input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username or email">                                        
-					</div>
-
-					<div style="margin-bottom: 25px" class="input-group">
-						<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-						<input id="login-password" type="password" class="form-control" name="password" placeholder="password">
-					</div>
-
-					<div class="input-group">
-						<div class="checkbox">
-							<label>
-								<input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
-							</label>
-						</div>
-					</div>
-
-
-					<div style="margin-top:10px" class="form-group">
-						<!-- Button -->
-
-						<div class="col-sm-12 controls">
-							<a id="btn-login" href="#" class="btn btn-success">Login  </a>
-							<a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
-
-						</div>
-					</div>
-
-
-					<div class="form-group">
-						<div class="col-md-12 control">
-							<div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-								Don't have an account! 
-								<a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
-									Sign Up Here
-								</a>
-							</div>
-						</div>
-					</div>    
-				</form>     
-
-
-
-			</div>                     
-		</div>  
+<div class="col-sm-12">
+	<!-- Tab panes -->
+	<div class="tab-content">
+		<div id="contact" role="tabpanel" class="tab-pane fade" >
+			<?php include "view_contact.php"; ?>
+		</div>
+		<div id="loginIn" role="tabpanel" class="tab-pane fade">
+			<?php include "view_login.php"; ?>
+		</div>
+		<div id="signup" role="tabpanel" class="tab-pane fade">
+			<?php include "view_signup.php"; ?>
+		</div>
 	</div>
+	<!-- End Tab panes -->
+</div>
--- view/header.php
+++ view/header.php
@@ -7,26 +7,28 @@
 	  <meta name="keywords" content="HTML,CSS,XML,JavaScript">
 	  <meta name="author" content="John Doe">
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="/projek_frontend/css/style.css">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="/projek_frontend/css/animate.css">
-	<link rel="stylesheet" type="text/css" href="/projek_frontend/font-awesome/css/font-awesome.css">
+	  <!-- Latest compiled and minified CSS -->
+	  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	  <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+	  <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+
 </head>
 <body>
 
-	<script type="text/javascript" src="/projek_frontend/js/jquery-3.3.1.min.js"></script>
-	<script type="text/javascript" src="/projek_frontend/js/bootstrap.min.js"></script>
+	<!-- JavaScript -->
+	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 	<script>
 		$(function () {
 			$('[data-tooltip="tooltip"]').tooltip()
 		});
 	</script>
+	<!-- end ./ -->
 
+	<!-- container body -->
 	<div class="container">
 		 <!-- Fixed navbar -->
 		 <nav class="navbar navbar-default navbar-fixed-top">
@@ -55,13 +57,20 @@
 		 			<ul class="nav navbar-nav">
 		 				<li class=""><a href="#">Products</a></li>
 		 				<li><a href="#about">How it works</a></li>
-		 				<li><a href="#contact">FaQ</a></li>
-		 				<li><a href="../navbar/">Log In</a></li>
+		 				<li>
+		 					<a href="#contact" aria-controls="contact" role="tab" data-toggle="tab" style="background:#0D69B7;" >FaQ</a>
+		 				</li>
+		 				<li>
+		 					<a href="#loginIn" aria-controls="loginIn" role="tab" data-toggle="tab" style="background:#0D69B7;">Log In</a>
+		 				</li>
 		 			</ul>
 		 		</div><!--/.nav-collapse -->
 		 	</div>
-		 </nav>
+		</nav>
+		<!-- end </nav> -->
 	</div>
+	<!-- / end container -->
+	
 	
 
 	
