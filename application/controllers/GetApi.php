79ba425ed7d6dcfbeeb9e173cfc8b5a13d249ad6
budiamanprasetyo <setyaamanbudi@gmail.com>
2018-02-10 23:43:21 +0700

    add controller GetApi

---
+++ application/controllers/GetApi.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class GetApi extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->load->library( array('session','encrypt') );
+	}
+
+	public function get_user()
+	{
+	}
+
+	public function post_user()
+	{
+
+	}
+
+	public function delete_user()
+	{
+
+	}
+
+	public function put_user()
+	{
+
+	}
+
+	public function en_decode()
+	{
+		if ( is_null($data)) {
+			echo json_encode( array("error"=>1, "msg"=>"Data Fail") );
+			return false;
+		}
+
+		$data = $this->input->post("params", TRUE);
+		$key = "banana";
+		$method ="AES-256-ECB";
+
+		$result = openssl_decrypt ($data, $method, $key);
+		echo json_encode( array("error"=>0, "msg"=>$result) );
+		return true;
+	}
+
+
+
+}
+
+/* End of file GetApi.php */
+/* Location: ./application/controllers/GetApi.php */
