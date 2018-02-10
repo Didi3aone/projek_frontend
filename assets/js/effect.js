60d3c2f32b37e00798c944359c79ab5b9a441f7e
Didi Triawan <diditriawan13@gmail.com>
2018-02-10 19:32:24 +0700

    update

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -1,54 +1,75 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+  if(!$this.hasClass('panel-collapsed')) {
+    $this.parents('.panel').find('.panel-body').slideUp();
+    $this.addClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+  } else {
+    $this.parents('.panel').find('.panel-body').slideDown();
+    $this.removeClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+  }
+})
 
-    });
-	var acc = document.getElementsByClassName("accordion");
-	var i;
-
-	for (i = 0; i < acc.length; i++) {
-	    acc[i].onclick = function() {
-	    this.classList.toggle("active");
-	    var panel = this.nextElementSibling;
-	    if (panel.style.maxHeight){
-	      panel.style.maxHeight = null;
-	    } else {
-	      panel.style.maxHeight = panel.scrollHeight + "px";
-	    } 
-    }
-
-    popup = {
-      init: function(){
-        $('figure').click(function(){
-          popup.open($(this));
-        });
-        
-        $(document).on('click', '.popup img', function(){
-          return false;
-        }).on('click', '.popup', function(){
-          popup.close();
-        })
-      },
-      open: function($figure) {
-        $('.gallery').addClass('pop');
-        $popup = $('<div class="popup" />').appendTo($('body'));
-        $fig = $figure.clone().appendTo($('.popup'));
-        $bg = $('<div class="bg" />').appendTo($('.popup'));
-        $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>').appendTo($fig);
-        $shadow = $('<div class="shadow" />').appendTo($fig);
-        src = $('img', $fig).attr('src');
-        $shadow.css({backgroundImage: 'url(' + src + ')'});
-        $bg.css({backgroundImage: 'url(' + src + ')'});
-        setTimeout(function(){
-          $('.popup').addClass('pop');
-        }, 10);
-      },
-      close: function(){
-        $('.gallery, .popup').removeClass('pop');
-        setTimeout(function(){
-          $('.popup').remove()
-        }, 100);
+var dropzone = new Dropzone('#demo-upload', {
+  previewTemplate: document.querySelector('#preview-template').innerHTML,
+  parallelUploads: 2,
+  thumbnailHeight: 120,
+  thumbnailWidth: 120,
+  maxFilesize: 3,
+  filesizeBase: 1000,
+  thumbnail: function(file, dataUrl) {
+    if (file.previewElement) {
+      file.previewElement.classList.remove("dz-file-preview");
+      var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+      for (var i = 0; i < images.length; i++) {
+        var thumbnailElement = images[i];
+        thumbnailElement.alt = file.name;
+        thumbnailElement.src = dataUrl;
       }
+      setTimeout(function() { file.previewElement.classList.add("dz-image-preview"); }, 1);
     }
+  }
 
-    popup.init()
+});
+
+
+// Now fake the file upload, since GitHub does not handle file uploads
+// and returns a 404
+
+var minSteps = 6,
+    maxSteps = 60,
+    timeBetweenSteps = 100,
+    bytesPerStep = 100000;
+
+dropzone.uploadFiles = function(files) {
+  var self = this;
+
+  for (var i = 0; i < files.length; i++) {
+
+    var file = files[i];
+    totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+
+    for (var step = 0; step < totalSteps; step++) {
+      var duration = timeBetweenSteps * (step + 1);
+      setTimeout(function(file, totalSteps, step) {
+        return function() {
+          file.upload = {
+            progress: 100 * (step + 1) / totalSteps,
+            total: file.size,
+            bytesSent: (step + 1) * file.size / totalSteps
+          };
+
+          self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
+          if (file.upload.progress == 100) {
+            file.status = Dropzone.SUCCESS;
+            self.emit("success", file, 'success', null);
+            self.emit("complete", file);
+            self.processQueue();
+            //document.getElementsByClassName("dz-success-mark").style.opacity = "1";
+          }
+        };
+      }(file, totalSteps, step), duration);
+    }
+  }
 }
